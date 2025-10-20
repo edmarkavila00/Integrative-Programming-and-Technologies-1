b70a7f2f2f772f51cffb1dac3d0b9e5775e3a8ef
ID 10 T <edmarkavila25@gmail.com>
2025-10-20 21:59:19 +0800

    lab1_encode.php

---
+++ Finals/lab1_encode.php
@@ -0,0 +1,12 @@
+<?php
+
+$student = array(
+    "name" => "Edmark Lupiga",
+    "age" => 21,
+    "course" => "BS Information Technology"
+);
+
+$json_data = json_encode($student);
+
+echo $json_data;
+?>
